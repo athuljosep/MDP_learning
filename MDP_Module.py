fbad6a010905971871de0e335b7dc8fc2a4514f0
athuljosep <70621782+athuljosep@users.noreply.github.com>
2024-07-09 18:10:04 -0700

    policy extraction added

--- MDP_Module.py
+++ MDP_Module.py
@@ -31,30 +31,27 @@ def value_iteration(states, actions, P, R, gamma, theta, n_iter):
 
 # Policy Iteration Function
 def policy_iteration(states, actions, P, R, gamma, theta, n_iter):
-
     # Initializing values as zeros
     V = {s: 0 for s in states}
     V_history  = []
-
     for i in range (1, n_iter):
-        
         delta = 0
-        
         for s in states:
-            
             v = V[s]
             a = pi(s)
             V[s] = sum(P[(s, a)][s_prime] * (R[(s, a)][s_prime] + gamma * V[s_prime]) for s_prime in states)
             delta = max(delta, abs(v - V[s]))
-        
         V_history.append(list(V.items()))
-
-
         if delta < theta:
-            
             break
-        
         i = i+1
 
+    policy = {}
+    for s in states:
+        action_values = {}
+        for a in actions:
+            action_values[a] = sum(P[(s, a)][s_prime] * (R[(s, a)][s_prime] + gamma * V[s_prime]) 
+                                   for s_prime in states)
+        policy[s] = max(action_values, key=action_values.get)
 
-    return V_history
+    return V_history, policy
